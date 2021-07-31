4d6557357486f7fc62001fea231ecd8c23e68a08
落鸿挽涟漪 <2892537581@qq.com>
2021-07-31 20:58:33 +0800

    2021-7-31 21:00

--- src/plugins/vant.js
+++ src/plugins/vant.js
@@ -1,33 +1,37 @@
 // 按需全局引入 vant组件
-import Vue from 'vue'
+import Vue from 'vue';
 import {
-  Button,
-  List,
-  Cell,
-  Tabbar,
-  TabbarItem,
-  Row,
-  Col,
-  Icon,
-  Image,
-  Card,
-  PasswordInput,
-  NumberKeyboard,
-  Divider,
-  Grid,
-  GridItem
-} from 'vant'
-Vue.use(Button)
-Vue.use(Cell)
-Vue.use(List)
-Vue.use(Tabbar).use(TabbarItem)
-Vue.use(Row)
-Vue.use(Col)
-Vue.use(Icon)
-Vue.use(Image)
-Vue.use(Card)
-Vue.use(PasswordInput)
-Vue.use(NumberKeyboard)
-Vue.use(Divider)
-Vue.use(Grid)
-Vue.use(GridItem)
+    Button,
+    List,
+    Cell,
+    Tabbar,
+    TabbarItem,
+    Row,
+    Col,
+    Icon,
+    Image,
+    Card,
+    PasswordInput,
+    NumberKeyboard,
+    Divider,
+    Grid,
+    GridItem,
+    Skeleton,
+    Empty
+} from 'vant';
+Vue.use(Button);
+Vue.use(Cell);
+Vue.use(List);
+Vue.use(Tabbar).use(TabbarItem);
+Vue.use(Row);
+Vue.use(Col);
+Vue.use(Icon);
+Vue.use(Image);
+Vue.use(Card);
+Vue.use(PasswordInput);
+Vue.use(NumberKeyboard);
+Vue.use(Divider);
+Vue.use(Grid);
+Vue.use(GridItem);
+Vue.use(Skeleton);
+Vue.use(Empty);

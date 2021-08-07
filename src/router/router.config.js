deee35d42a99418bee893b4f8489aae7348d5d74
落鸿挽涟漪 <2892537581@qq.com>
2021-08-07 17:41:58 +0800

    合并了最新的master分支，approveAct有一个大bug未解决

--- src/router/router.config.js
+++ src/router/router.config.js
@@ -1,57 +1,70 @@
 export const constantRouterMap = [
-  // 主页面
-  {
-    path: '/',
-    component: () => import('@/views/Home/index'),
-    meta: {
-      title: '志愿者服务',
-      keepAlive: false
-    }
-  },
-  // 提交工时
-  {
-    path: '/approveAct',
-    name: 'ApproveAct',
-    component: () => import('@/views/approveAct'),
-    meta: { title: '工时提交 - 志愿者服务', keepAlive: false }
-  },
-  {
-    path: '/test',
-    component: () => import('@/views/Test.vue')
-  },
-  // 活动招募页面
-  {
-    path: '/actrecruit',
-    component: () => import('@/views/ActRecruit'),
-    meta: {
-      title: '志愿者服务-活动招募',
-      keepAlive: false
-    }
-  },
-  // 信用等级页面
-  {
-    path: '/creditlevel',
-    component: () => import('@/views/CreditLevel')
-  },
-  // 工时概览页面
-  {
-    path: '/mytimes',
-    component: () => import('@/views/MyVolTime')
-  },
-  // 违规举报页面
-  {
-    path: '/reasons',
-    component: () => import('@/views/report/Reasons')
-  },
-  // 举报记录页面
-  {
-    path: '/reportlist',
-    component: () => import('@/views/ReportList')
-  },
-  // 404页面
-  {
-    path: '*',
-    name: 'NotFound',
-    component: () => import('@/views/NotFound.vue')
-  }
+	// 主页面
+	{
+		path: '/',
+		component: () => import('@/views/Home/index'),
+		meta: {
+			title: '志愿者服务',
+			keepAlive: false
+		}
+	},
+	// 提交工时
+	{
+		path: '/approveAct',
+		name: 'ApproveAct',
+		component: () => import('@/views/approveAct'),
+		meta: { title: '工时提交 - 志愿者服务', keepAlive: false }
+	},
+	{
+		path: '/test',
+		component: () => import('@/views/Test.vue')
+	},
+	// 活动招募页面
+	{
+		path: '/actrecruit',
+		component: () => import('@/views/ActRecruit'),
+		meta: {
+			title: '志愿者服务-活动招募',
+			keepAlive: false
+		}
+	},
+	// 信用等级页面
+	{
+		path: '/creditlevel',
+		component: () => import('@/views/CreditLevel')
+	},
+	// 工时概览页面
+	{
+		path: '/mytimes',
+		component: () => import('@/views/MyVolTime')
+	},
+	// 违规举报页面---举报-1
+	{
+		path: '/reasons',
+		component: () => import('@/views/report/Reasons'),
+		meta: {
+			title: '举报原因',
+			keepAlive: false
+		}
+	},
+	// 举报信息详情---举报-2
+	{
+		path: '/details',
+		component: () => import('@/views/report/Details'),
+		meta: {
+			title: '举报原因',
+			keepAlive: false
+		}
+	},
+	// 举报记录页面
+	{
+		path: '/reportlist',
+		component: () => import('@/views/ReportList')
+	},
+	// 404页面
+	{
+		path: '*',
+		name: 'NotFound',
+		component: () => import('@/views/NotFound.vue')
+	}
 ];
---
+++ src/views/report/Details.vue
@@ -0,0 +1,54 @@
+<template>
+	<div>
+		<!-- <h1 class="text-center">违规举报页面</h1> -->
+		<van-nav-bar
+			title="举报详情"
+			left-text="返回"
+			fixed
+			placeholder
+			left-arrow
+			@click-left="$router.go(-1)"
+			@click-right="onClickRight"
+		>
+			<template #right>
+				<van-button plain type="info" size="mini" class="fs-xxxxs">提交</van-button>
+			</template>
+		</van-nav-bar>
+		<van-notice-bar color="#1989fa" background="#ecf9ff" scrollable left-icon="info-o">
+			此次举报将全程保护您的个人信息，请实事求是、放心填写！
+		</van-notice-bar>
+
+		<van-cell-group>
+			<van-cell title="单元格" value="" />
+		</van-cell-group>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			palceholder: '请输入具体原因',
+			reasons: ['做与志愿服务活动无关的事情', '进行影响活动开展的个人活动', '迟到', '早退', '旷到'],
+			result: [],
+			otherrason: ''
+		};
+	},
+	methods: {
+		onClickRight() {
+			if (this.otherrason === '') {
+				console.log('结果', this.result);
+			} else {
+				this.result.push(this.otherrason);
+				console.log('结果', this.result);
+			}
+			this.$router.push('/details');
+		},
+		toggle(index) {
+			this.$refs.checkboxes[index].toggle();
+		}
+	}
+};
+</script>
+
+<style></style>

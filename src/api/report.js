cfbc83107a1999b3509365741280552a7cb99548
albatross <favoritettt@qq.com>
2021-08-15 19:49:33 +0800

    1

--- src/api/report.js
+++ src/api/report.js
@@ -1,18 +1,13 @@
 import request from './request';
 
 // 提交举报信息
-function commitReport(params, files) {
-	const form = new FormData();
-	console.log(files);
-	for (let i = 0; i < files.length; i++) {
-		form.append('file', files[i].file);
-	}
+function commitReport(params, data) {
 	return request({
 		url: '/inform/insert',
 		method: 'POST',
 		hideloading: true, // vant 加载效果
 		params,
-		data: form
+		data
 	});
 }
 export { commitReport };
--- src/views/report/Details.vue
+++ src/views/report/Details.vue
@@ -112,21 +112,30 @@ export default {
 	methods: {
 		async onSubmit() {
 			this.details.informPersonNum = this.$store.state.userInfo.studentNum;
-			
+
 			const form = new FormData();
 			for (let i = 0; i < this.reportPhotos.length; i++) {
 				form.append('file', this.reportPhotos[i].file);
 			}
-			let res = await commitReport(this.details, form);
-			if (res.code === 0) {
-				this.$toast.success('提交举报成功!');
-			} else {
-				this.$toast.fail(res.msg);
+
+			for (var key of form.keys()) {
+				console.log('key:' + key + ' value:' + form.get(key));
+				let obj = form.get(key);
+				for(var key in obj) {
+					console.log(key, obj[key]);
+				}
 			}
+
+			// let res = await commitReport(this.details, form);
+			// if (res.code === 0) {
+			// 	this.$toast.success('提交举报成功!');
+			// } else {
+			// 	this.$toast.fail(res.msg);
+			// }
 			// 提交成功回到首页
-			setTimeout(() => {
-				this.$router.push('/');
-			}, 1500);
+			// setTimeout(() => {
+			// 	this.$router.push('/');
+			// }, 1500);
 		},
 		StartTimeConfirm(time) {
 			this.details.startTime =

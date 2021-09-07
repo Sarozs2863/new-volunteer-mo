528e01ef353e1b21a41b2012aa7775eae13c718c
落鸿挽涟漪 <2892537581@qq.com>
2021-09-07 21:28:44 +0800

    完善了我的工时部分

--- src/views/MyVolTime.vue
+++ src/views/MyVolTime.vue
@@ -17,25 +17,30 @@
 				<van-col>{{ $store.state.userInfo.studentNum }}</van-col>
 			</van-row> -->
 			<van-row class="Item-details">
+				<img src="../assets/img/myact.png" width="35px" />
 				<van-col>已参加活动：</van-col>
 				<van-col>{{ $store.state.hourView.activityJoined }}</van-col>
 			</van-row>
 			<van-row class="Item-details">
+				<img src="../assets/img/confirmed.png" width="35px" />
 				<van-col>已认证工时：</van-col>
 				<van-col>{{ $store.state.hourView.timePassed }}h</van-col>
 			</van-row>
 			<van-row class="Item-details">
+				<img src="../assets/img/unconfirmed.png" width="35px" />
 				<van-col>未认证工时：</van-col>
 				<van-col>{{ $store.state.hourView.timeToBePassed }}h</van-col>
-			</van-row>
-			<van-row>
-				<!-- <ActCard v-if="$store.state.recentActs.length > 0" :actList="$store.state.recentActs"></ActCard> -->
 			</van-row>
 			<div class="tips">
 				快去参加更多志愿活动吧~
 			</div>
 		</div>
-
+		<!-- <br /> -->
+		<ActCard
+			style="background-color:rgb(248, 252, 255)"
+			v-if="$store.state.recentActs.length > 0"
+			:actList="$store.state.recentActs"
+		></ActCard>
 		<copyright style="z-index:1000"></copyright>
 	</div>
 </template>
@@ -55,19 +60,19 @@ export default {
 .wrap {
 	background-color: rgb(248, 252, 255);
 	.sumcard {
-		border-radius: 0.6rem;
+		border-radius: 0.3rem;
 		background-color: #fff;
 		padding: 10px 20px 10px 10px;
 		box-shadow: 0 8px 12px #ebedf0;
-		margin: 0px 0.5rem;
-		background-color: rgb(231, 243, 251);
+		margin: 0px 0.4rem;
+		// background-color: rgb(231, 243, 251);
 	}
 	.myTimes {
-		height: 300px;
-		margin-top: 20px;
+		height: 200px;
+		margin-top: 8px;
 		padding-top: 20px;
 		.Item-details {
-			margin: 10px 10px 10px 24px;
+			margin: 4px 10px 10px 24px;
 			line-height: 40px;
 			border-bottom: grey solid 1px;
 			font-family: '华文中宋';
